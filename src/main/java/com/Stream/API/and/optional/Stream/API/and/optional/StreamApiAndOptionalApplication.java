f5eaf2c2c066253f4aaae3c57155670859e4a2f2
Arty <kirya26763@mail.ru>
2025-04-11 15:03:15 +0700

    start project

---
+++ src/main/java/com/Stream/API/and/optional/Stream/API/and/optional/StreamApiAndOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.Stream.API.and.optional.Stream.API.and.optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiAndOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiAndOptionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Stream/API/and/optional/Stream/API/and/optional/StreamApiAndOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stream.API.and.optional.Stream.API.and.optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiAndOptionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
