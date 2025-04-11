7c417dbe7bfd170d2720406065b8352bebc61e30
Arty <kirya26763@mail.ru>
2025-04-11 15:43:36 +0700

    edit

--- src/main/java/com/Stream/API/and/optional/Service/EmployeeService.java
+++ src/main/java/com/Stream/API/and/optional/Service/EmployeeService.java
@@ -1,4 +1,32 @@
 package com.Stream.API.and.optional.Service;
 
+import com.Stream.API.and.optional.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class EmployeeService {
+    private final Map<String, Employee> employees;
+
+    public EmployeeService() {
+        employees = new HashMap<>(Map.of(
+                "Дмитрий", new Employee("Дмитрий", "Иванов", "Александрович", 152100, "1"),
+                "Олег", new Employee("Олег", "Дмитрев", "Евгеневич", 152000, "1"),
+                "Вадим", new Employee("Вадим", "Стрельников", "Никитич", 100020, "2"),
+                "Александр", new Employee("Александр", "Рубинов", "Леонидович", 100000, "2"),
+                "Данил", new Employee("Данил", "Михаилов", "Генадьевичь", 80007, "3"),
+                "Леонид", new Employee("Леонид", "Григорьев", "Артемович", 55000, "3"),
+                "Алексей", new Employee("Алексей", "Матросов", "Михаилович", 60001, "4"),
+                "Михаил", new Employee("Михаил", "Берегов", "Алексеевич", 60000, "4"),
+                "Кирилл", new Employee("Кирилл", "Артемьев", "Владимирович", 45097, "5"),
+                "Никита", new Employee("Никита", "Голубцов", "Григорьевичь", 45000, "5")));
+    }
+
+    public List<Employee> allEmployee() {
+        return new ArrayList<>(employees.values());
+    }
 }
--- src/main/java/com/Stream/API/and/optional/StreamApiAndOptionalApplication.java
+++ src/main/java/com/Stream/API/and/optional/StreamApiAndOptionalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StreamApiAndOptionalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamApiAndOptionalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiAndOptionalApplication.class, args);
+    }
 
 }
